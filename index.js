e21e96f41cba104ab3b41da73fa8692efff6d113
Aldo Mata <gamboaldo@gmail.com>
2021-06-20 14:08:02 -0700

    wip

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const server = require("./api/server");
 
 const port = process.env.PORT || 2000;
 
-server.listen(2000, () => {
-  console.log(`\n Listening on port ${2000} \n`);
+server.listen(port, () => {
+  console.log(`\n Listening on port ${port} \n`);
 });
